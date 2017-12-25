8e763d729a14d7c15369f0c999ee453f3ec4b489
tangtang <940289231@qq.com>
2017-12-25 10:47:09 +0800

    Initial Commit

---
+++ DJS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DJS
+//
+//  Created by mac on 2017/12/25.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
